499c02ae5b9c0fad79496905d0bebe69c467b516
Endernah <argundeyvi@gmail.com>
2024-01-17 18:57:41 -0600

    modified:   src/dependency_installer.py

--- src/dependency_installer.py
+++ src/dependency_installer.py
@@ -3,16 +3,26 @@ if args.debug:
     debug = ""
 else:
     debug = "> /dev/null 2>&1"
-print("Installing dependencies")
 import os, sys, subprocess
-version = subprocess.check_output(['python3', '--version'], text=True)
-if not "3" in f"{version}":
-    print("Python 3 not found")
+print("Checking for Python3")
+try:
+    version = subprocess.check_output(['python3', '--version'], text=True)
+except:
+    print("Failed to check for Python3, Exiting!")
     exit()
+if not "3" in f"{version}":
+    print("Python3 not found")
+    exit()
+else:
+    if args.debug:
+        print(f"Python3 found: {version}")
+    else:
+        print("Python3 found")
+print("Installing dependencies")
 try:
     os.system(f"python3 -m pip install --upgrade pip {debug}")
     os.system(f"python3 -m pip install wget {debug}")
     os.system(f"python3 -m pip install requests {debug}")
 except:
-    print("Failed to install dependencies, Error: 202")
+    print("Failed to install dependencies, Exiting!")
     exit()
