fc7e165cac312656ef7a3aeda15640fe6dad5a28
Endernah <argundeyvi@gmail.com>
2024-01-18 11:28:09 -0600

    modified:   src/dependency_installer.py 	new file:   src/logs/2024-01-18-2.log.gz 	modified:   src/logs/latest.log

--- src/dependency_installer.py
+++ src/dependency_installer.py
@@ -7,9 +7,17 @@ import os, sys, subprocess
 print("Checking for Python3")
 try:
     version = subprocess.check_output(['python3', '--version'], text=True)
+    python = "python3"
 except:
-    print("Failed to check for Python3, Exiting!")
-    exit()
+    try:
+        version = subprocess.check_output(['python', '--version'], text=True)
+        python = "python"
+    except:
+        print("Failed to check for Python3, Exiting!")
+        exit()
+    if not python:
+        print("Failed to check for Python3, Exiting!")
+        exit()
 if not "3" in f"{version}":
     print("Python3 not found")
     exit()
@@ -20,10 +28,10 @@ else:
         print("Python3 found")
 print("Installing dependencies")
 try:
-    os.system(f"python3 -m pip install --upgrade pip {debug}")
-    os.system(f"python3 -m pip install wget {debug}")
-    os.system(f"python3 -m pip install requests {debug}")
-    os.system(f"python3 -m pip install minecraft-launcher-lib {debug}")
+    os.system(f"{python} -m pip install --upgrade pip {debug}")
+    os.system(f"{python} -m pip install wget {debug}")
+    os.system(f"{python} -m pip install requests {debug}")
+    os.system(f"{python} -m pip install minecraft-launcher-lib {debug}")
 except:
     print("Failed to install dependencies, Exiting!")
     exit()
